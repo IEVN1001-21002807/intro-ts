bcd48244de34db99449bc61e20bb158beec9232d
Citla <gutierrezcitlali22@gmail.com>
2024-09-09 22:02:38 -0600

    primer commit con intro ts, Lunes 9

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,24 @@
+//console.log('Hola Mundo')
+
+var nombre = 'Juan'
+let apellido = 'Perez'
+const edad = 30
+
+let num1=2
+let num2='5yryryry'
+let num3=3
+
+let suma = num1 + num2 + num3
+console.log(suma)
+
+let correo:string;
+let edad2:number;
+let activo:boolean
+
+correo = 'citlalokota@gmail.com'
+edad2 = 12
+activo = true
+
+let matricula:number|string|boolean;
+matricula = 21002807
+matricula = '1236547'
